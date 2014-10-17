17d9c482f86e2033625bfe1e4e920236fcec4f09
danse <f.occhipinti@gmail.com>
2014-10-17 13:25:33 +0200

    add validation logic

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-data_model
-==========
+# Standard Data Models
+
+This repository provides examples and validators for data models that
+we want to share through different eHealth projects
+
+Check the [jjv doc][jjv] in order to have more info about how to
+specify a schema.
+
+## How to use this
+
+Add the dependency from the module, then use the provided `validate`
+method. For example if you want to validate a `Person` object:
+
+    var dataModel = require('data-model');
+    var person = { ... };
+    var errors = dataModel.validate(person);
+
+If the object is valid, errors should be `null`.
+
+[jjv]: https://github.com/acornejo/jjv
---
+++ test/test.js
@@ -0,0 +1,29 @@
+var assert = require('assert'),
+    dataModel = require('../index');
+
+describe('person', function() {
+  var example;
+  beforeEach(function() {
+    example = require('./examples/person/1.json');
+  });
+  it('validates the first example', function() {
+    var errors = dataModel.validate(example);
+    assert.equal(errors, null, 'Errors are '+JSON.stringify(errors));
+  });
+  it('complains when the doc_type is missing', function() {
+    var errors = dataModel.validate({wrong: 'stuff'});
+    assert.equal(
+      errors.dataModel,
+      'the object to be validated is missing a `doc_type` property'
+    );
+  });
+  it('complains when the doc_type has an unknown value', function() {
+    assert.throws(function() {
+      dataModel.validate({any: 'shit', doc_type: 'cat'});
+    }, /could not find schema 'cat'/);
+  });
+  it('complains when required fields are missing', function() {
+    var errors = dataModel.validate({wrong: 'stuff', doc_type: 'Person'});
+    assert.equal(errors.validation.surname.required, true);
+  });
+});
