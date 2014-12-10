864bcca2fe53558e4a6396195addd7b02b83e114
Tom Vincent <git@tlvince.com>
2014-12-10 18:44:21 +0000

    Add driver required fields test

--- test/driver.js
+++ test/driver.js
@@ -10,4 +10,14 @@ describe('driver', function() {
     var errors = dataModel.validate(example1);
     assert.equal(errors, null, JSON.stringify(errors));
   });
+
+  it('fails if some required fields are missing', function() {
+    var errors = dataModel.validate({
+      /*eslint-disable camelcase */
+      doc_type: 'driver'
+      /*eslint-enable camelcase */
+    });
+    var hasErrors = errors.length > 0;
+    assert.equal(hasErrors, true, 'Driver schema is missing required fields');
+  });
 });
