b2719b60df06494e0034bad20e461d62dc163eba
Tom Vincent <git@tlvince.com>
2015-03-26 11:16:44 +0000

    test(nutritionSurvey): team indexed by leader ID

--- test/nutrition-survey.js
+++ test/nutrition-survey.js
@@ -4,10 +4,16 @@ var assert = require('assert');
 var dataModel = require('../index');
 
 var example1 = require('./examples/nutrition-survey/1.json');
+var example2 = require('./examples/nutrition-survey/2.json');
 
 describe('nutrition-survey', function() {
   it('validates the first example', function() {
     var errors = dataModel.validate(example1);
+    assert.equal(errors, null, JSON.stringify(errors, null, 2));
+  });
+
+  it('handles teams indexed by a team leader', function() {
+    var errors = dataModel.validate(example2);
     assert.equal(errors, null, JSON.stringify(errors, null, 2));
   });
 });
