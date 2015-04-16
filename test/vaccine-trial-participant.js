7f9aa04c25f44364174a04202a4ae4cb4c12881b
Karl Westin <karl.westin@gmail.com>
2015-04-16 12:00:42 +0200

    update vaccine trial model

--- test/vaccine-trial-participant.js
+++ test/vaccine-trial-participant.js
@@ -6,8 +6,13 @@ var example1 = require('./examples/vaccine-trial-participant/1.json');
 var example2 = require('./examples/vaccine-trial-participant/2.json');
 
 describe('vaccineTrialParticipant', function() {
-  it('validates valid example succesfully', function() {
+  it('validates small example succesfully', function() {
     var errors = dataModel.validate(example1);
+    assert.equal(errors, null, JSON.stringify(errors));
+  });
+
+  it('validates big example', function() {
+    var errors = dataModel.validate(example2);
     assert.equal(errors, null, JSON.stringify(errors));
   });
 });
