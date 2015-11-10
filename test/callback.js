70ddc94bd67888573bf28d2492aab13e3751091f
danse <f.occhipinti@gmail.com>
2015-11-10 12:19:17 +0100

    add a `cancelled` status for callbacks - eHealthAfrica/sl-ebola-call-admin#1091

--- test/callback.js
+++ test/callback.js
@@ -4,10 +4,15 @@ var assert = require('assert');
 var dataModel = require('../index');
 
 var example1 = require('./examples/callback/1.json');
+var example2 = require('./examples/callback/2.json');
 
 describe('callback', function() {
   it('validates the first example', function() {
     var errors = dataModel.validate(example1);
+    assert.equal(errors, null, JSON.stringify(errors));
+  });
+  it('validates the second example', function() {
+    var errors = dataModel.validate(example2);
     assert.equal(errors, null, JSON.stringify(errors));
   });
 });
